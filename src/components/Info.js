5b09a157be7c90363d2b278d2ea8832c87405842
jawwad-ali <alijawwad001@gmail.com>
2022-11-17 18:28:04 +0500

    globe section responsive

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,43 +4,42 @@ import LineImageOne from "./assets/line_image_one.png"
 import LineImageThree from "./assets/line_image_three.png"
 import RightArrow from "./assets/right_arrow.png"
 
-const ThirdParaBoldText = <span className="font-extrabold text-black">decentralized</span>
-console.log(ThirdParaBoldText)
 const Info = () => {
     return (
-        <div className='w-full bg-[#31B9B5] flex h-[200vh]'>
-            <div className='w-3/6 flex flex-col justify-between ml-20 py-20'>
-                <div className=' w-[100%] flex flex-col justify-between h-[200vh] '>
-                    <InfoChild 
-                        img={LineImageOne}
-                        boldText={"Enhancing value"}
-                        text={"across business Runways through innovative and design led data acquisition & management tools"}
-                        btn="Learn More" 
-                    />
-
-                    <InfoChild
-                        img={LineImageOne}
-                        text={"We aggregate real time data & support service exchange to enable actors achieve goals across value and supply chains."}
-                        btn="Learn More"
-                    />
+        <div className='w-full bg-[#31B9B5] flex xl:flex-row flex-col xl:h-[200vh]'>
+            <div className='w-full xl:w-3/6 flex xl:flex-col flex-row justify-between ml-0 md:ml-9 lg:ml-12 py-20'>
+                <div className='w-[100%] md:ml-4 md:w-[93%] lg:w-[90%] flex flex-col justify-between xl:h-[200vh]'>
+                    <div className='mb-8 '>
+                        <InfoChild
+                            img={LineImageOne}
+                            boldText={"Enhancing value"}
+                            text={"across business Runways through innovative and design led data acquisition & management tools"}
+                            btn="Learn More"
+                        />
+                    </div>
+                    <div>
+                        <InfoChild
+                            img={LineImageOne}
+                            text={"We aggregate real time data & support service exchange to enable actors achieve goals across value and supply chains."}
+                            btn="Learn More"
+                        />
+                    </div>
                 </div>
             </div>
 
-
-            <div className='w-3/6 flex flex-col justify-between py-20 '>
-                <div className='flex flex-col justify-between my-auto'>
-
-                    <div className='flex w-[75%]'>
-                        <img src={LineImageThree} alt="Line Image" />
-                        <div className=' ml-5 relative'>
-                            <p className='font-[poppins] text-black/[.8] font-light text-3xl leading-[63px]'>
+            <div className='w-full xl:w-3/6 flex flex-col justify-between xl:-mt-4 md:-mt-32 -mt-28  md:ml-9 lg:ml-12 py-20 ml-3'>
+                <div className='flex flex-col justify-between my-auto md:ml-4 md:w-[93%]'>
+                    <div className='flex '>
+                        <img src={LineImageThree} alt="Line Image" className='h-[220px] md:h-[220px] lg:h-[180px] xl:h-[400px]' />
+                        <div className='ml-5 md:ml-2 relative'>
+                            <p className='font-[poppins] text-black/[.8] text-xl leading-[35px] xl:text-3xl font-light md:leading-[55px] xl:leading-[63px]'>
                                 We facilitate a <span className='font-semibold text-black'>decentralized</span>, yet holistic agent network built on easy-to-use conversational platforms. We connect value creators across business runways to facilitate real-time access to key services & data.
                             </p>
                         </div>
                     </div>
-                    <div className='ml-14 mt-5'>
+                    <div className='xl:ml-14 ml-8 md:ml-7 md:mt-3 xl:mt-2 mb-5 '>
                         <a href="#" >
-                            <button className='bg-[#051641] rounded-md font-[Light] text-white w-[230px] h-12 flex items-center justify-center'>Learn More
+                            <button className='bg-[#051641] rounded-md font-[Light] text-white xl:w-[230px] w-[200px] h-10 xl:h-12 flex items-center justify-center'>Learn More
                                 <img src={RightArrow} className="ml-5" />
                             </button>
                         </a>
--- src/components/InfoChild.js
+++ src/components/InfoChild.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import RightArrow from "./assets/right_arrow.png"
-import LineImageOne from "./assets/line_image_one.png"
-import LineImage from "./assets/line_image_three.png"
 
 const InfoChild = ({ img, boldText, text, btn }) => {
     return (
-        <div className='w-[75%]'>
+        <div className='w-full xl:w-[75%] ml-2'>
             <div className='flex'>
-                <img src={img} alt="Line Image" />
-                <div className='ml-5 relative w-[100%]'>
-                    <p className='font-[poppins]   text-black/[.8] font-light text-3xl leading-[63px]'>
+                <img src={img} alt="Line Image" className='h-[150px] md:h-[120px] lg:h-[150px] xl:h-[400px]' />
+                <div className='ml-3 md:ml-2 relative w-[90%] '>
+                    <p className='font-[poppins] text-black/[.8] font-light text-xl xl:text-3xl leading-[35px] md:leading-[55px] xl:leading-[63px]'>
                         <span className='font-semibold text-black'>{boldText}</span> {text}
                     </p>
                 </div>
             </div>
-            <div className='ml-14 mt-2'>
+            <div className='xl:ml-14 ml-9 md:ml-7 md:mt-0 mt-2 mb-5'>
                 <a href="#" >
-                    <button className='bg-[#051641] rounded-md font-[Light] text-white w-[230px] h-12 flex items-center justify-center'>
+                    <button className='bg-[#051641] rounded-md font-[Light] text-white xl:w-[230px] w-[200px] h-10 xl:h-12 flex items-center justify-center'>
                         {btn}
                         <img src={RightArrow} className="ml-5" />
                     </button>
