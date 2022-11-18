4165755a676c54236d728170c605eb44cfd3e1c9
jawwad-ali <alijawwad001@gmail.com>
2022-11-18 22:34:51 +0500

    minor changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,19 +13,19 @@ const Footer = () => {
                     <p className="text-[#f1f1f2] text-lg font-[Light] mt-5">talk to us</p>
                 </div> 
 
-                <div className="mt-10 xl:mt-0 xl:w-3/6">
+                <div className="mt-10 xl:mt-0 xl:w-3/6 bg-red-600">
                     <h3 className="font-[Light] font-medium text-[22px] text-[#f9f9f9]">Contact Us</h3>
-                    <div className="mt-3 flex flex-col xl:flex-row w-[75%] xl:w-full  justify-around md:justify-start -ml-5 md:ml-0">
+                    <div className="mt-3 flex flex-col xl:flex-row w-[75%] xl:w-full justify-around md:justify-start -ml-5 md:ml-0">
                         <div className="flex xl:flex-row justify-around md:justify-start my-2">
                             <img src={PinIcon} alt="Pin" />
                             <p className="pl-2 text-[#f9f9f9] text-lg font-[Proxima]">Limuru Rd, Nairobi</p>
                         </div>
                         <div className="flex justify-around my-2 md:justify-start lg:ml-0">
-                            <img src={PhoneIcon} alt="Phone Image" className="lg:ml-0 ml-3" />
+                            <img src={PhoneIcon} alt="Phone Image" className="lg:ml-3 ml-3" />
                             <p className="pl-2 text-[#f9f9f9] text-lg font-[Proxima]">+254 769 533 786</p>
                         </div>
                         <div className="flex justify-around my-2 md:justify-start lg:ml-0">
-                            <img src={EmailIcon} alt="Phone Image" className="lg:ml-0 ml-3" />
+                            <img src={EmailIcon} alt="Phone Image" className="lg:ml-3 ml-3" />
                             <p className="pl-2 text-[#f9f9f9] text-lg font-[Proxima]">mambo@spaceai.io</p>
                         </div>
                     </div>
@@ -33,7 +33,7 @@ const Footer = () => {
             </div>
 
             <div className="mt-16 flex items-center justify-between">
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between"> 
                     <img src={Copyright} alt="Copyright" />
                     <p className="text-[#31b9b5] text-base font-[Light] ml-2">Space Infinitus limited</p>
                 </div>
